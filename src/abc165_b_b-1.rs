a1215a3c46a3cc7b6e7e5f254793823225f9df8c
mshogo21 <mshogo0201@gmail.com>
2023-06-20 00:45:28 +0900

    abc165_b

---
+++ src/abc165_b_b-1.rs
@@ -0,0 +1,18 @@
+use proconio::input;
+
+fn main() {
+    input!{
+        x: usize,
+    }
+
+    let mut m = 100;
+    let mut ans = 0;
+    
+    while x > m {
+        m += m / 100;
+        ans += 1;
+    }
+
+    println!("{}",ans);
+}
+//https://atcoder.jp/contests/abc165/tasks/abc165_b
