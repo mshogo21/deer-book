b51dc3bf41694eb29abb2913a980247d1b72c1bf
mshogo21 <mshogo0201@gmail.com>
2023-06-20 21:18:59 +0900

    abc200 b

--- src/abc200_b.rs
+++ src/abc200_b.rs
@@ -1,9 +1,18 @@
+//https://atcoder.jp/contests/abc200/tasks/abc200_b
 use proconio::input;
 
 fn main() {
     input!{
-        a: usize
+        mut n: i64,
+        k: i64,
     }
 
-    println!("{}",a);
+    for _i in 1..=k {
+        if n % 200 == 0 {
+            n /= 200;
+        } else {
+            n = 1000 * n + 200;
+        }
+    }
+    println!("{}",n);
 }
